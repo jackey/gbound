43a71499e751f7ba37c34d265fe26b05cf383703
ziwen-web <jziwenchen@gmail.com>
2017-06-25 05:10:29 +0800

    .

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,36 @@ module.exports = function(app) {
 	// handle things like api calls
 	// authentication routes
 
+  app.get('/backend/*', function (req, res) {
+    console.log(req.url.replace('/backend', ''));
+    http.get({
+      hostname: '127.0.0.1',
+      port: '3000',
+      encoding: null,
+      path: req.url.replace('/backend', ''),
+      agent: false
+    }, function (apiRes) {
+      console.log('后台图片到手!');
+      var statusCode = apiRes.statusCode;
+      if (statusCode != 200) {
+        res.write('404');
+        apiRes.resume();
+      } else {
+        res.writeHead(200, {'Content-Type': 'image/png'});
+        apiRes.on('data', function (chunk) {
+          res.write(chunk);
+        });
+        apiRes.on('end', function() {
+          console.log('图片内容获取完毕!');
+          res.end();
+        });
+      }
+    });
+  }).on('error', function(e) {
+    console.log('获取后台图片失败');
+    res.send('404');
+  });
+
   app.get('/rest/*', function (req, res) {
 
 
